905d45e1e5312bcf25ffd12684bc0b31b862dd97
Cat <83475353+Vova-max-png@users.noreply.github.com>
2023-05-03 08:24:56 +0300

    feat: Delete unused imports

--- src/args.rs
+++ src/args.rs
@@ -1,11 +1,10 @@
-use std::path::PathBuf;
 use clap::Parser;
 
 /// Program to read docs
 #[derive(Parser, Debug)]
 #[command(author="Cat", version, about, long_about = None)]
 pub struct Args {
-    /// Url to page
+    /// Page url
     #[arg(default_value_t = String::from("https://google.com"))]
     pub url: String,
 }
